cd3beb6d9a1a8f1bb9a471918c2ce7073edcb985
Irene Bellani <bellani.irene@gmail.com>
2023-03-22 23:12:20 +0100

    Added current time and day

--- Weatherappfinal.js
+++ Weatherappfinal.js
@@ -1,3 +1,27 @@
+function currentTime() {
+  let date = new Date();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let daysoftheweek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = daysoftheweek[date.getDay()];
+  console.log(day);
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let degrees = document.querySelector("#degrees");
   degrees.innerHTML = Math.round(response.data.temperature.current);
@@ -10,8 +34,11 @@ function displayTemperature(response) {
   humidity.innerHTML = Math.round(response.data.temperature.humidity);
   let wind = document.querySelector("#wind");
   wind.innerHTML = Math.round(response.data.wind.speed);
+  let current = document.querySelector("#current");
+  current.innerHTML = currentTime();
 }
 
 let apiKey = "037a67b0fd6f93o58ea5b48t0191c6c9";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&&units=metric`;
+let city = "Lisbon";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
